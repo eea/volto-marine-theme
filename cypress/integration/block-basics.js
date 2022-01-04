5516f6262918d964a9bcb8d822aab3684e738563
kreafox <krisztina.n.elekes@gmail.com>
2022-01-04 09:57:43 +0200

    Update package.json & cypress

--- cypress/integration/block-basics.js
+++ cypress/integration/block-basics.js
@@ -6,13 +6,15 @@ describe('Blocks Tests', () => {
 
   it('Add Block: Empty', () => {
     // Change page title
-    cy.get('[contenteditable=true]').first().clear();
+    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
+      .clear()
+      .type('My Add-on Page')
+      .get('.documentFirstHeading span[data-text]')
+      .contains('My Add-on Page');
 
-    cy.get('[contenteditable=true]').first().type('My Add-on Page');
-
-    cy.get('.documentFirstHeading').contains('My Add-on Page');
-
-    cy.get('[contenteditable=true]').first().type('{enter}');
+    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block').type(
+      '{enter}',
+    );
 
     // Add block
     cy.get('.ui.basic.icon.button.block-add-button').first().click();
