808632952325336691b01ac52e91ac1e22e0b1a1
kreafox <krisztina.n.elekes@gmail.com>
2022-01-04 18:02:27 +0200

    Add scroll to top

---
+++ src/components/theme/AppExtras/index.js
@@ -0,0 +1,13 @@
+import ScrollToTop from './ScrollToTop';
+
+export default (config) => {
+  config.settings.appExtras = [
+    ...(config.settings.appExtras || []),
+    {
+      match: '',
+      component: ScrollToTop,
+    },
+  ];
+
+  return config;
+};
