1eabc3ed13d80ac1861d3b28fa2cf5d21c0f852e
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2022-09-27 16:20:04 +0300

    Fix tests

--- src/components/theme/DatabaseItemView/DatabaseItemView.jsx
+++ src/components/theme/DatabaseItemView/DatabaseItemView.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Icon } from 'semantic-ui-react';
 import { BodyClass } from '@plone/volto/helpers';
 import {
   ItemMetadataSnippet,
