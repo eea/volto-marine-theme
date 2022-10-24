e8fef8abffe1ae45fd1296d7614a58a3c861fb32
nileshgulia1 <nileshgulia@gmail.com>
2022-10-24 16:53:02 +0530

    fix plugins

--- razzle.extend.js
+++ razzle.extend.js
@@ -1,5 +1,20 @@
+const plugins = (defaultPlugins) => {
+  return defaultPlugins;
+};
+
 module.exports = {
-  modifyWebpackConfig({ webpackConfig }) {
+  plugins,
+  modifyWebpackConfig({
+    env: { target, dev },
+    webpackConfig,
+    webpackObject,
+    options: {
+      pluginOptions,
+      razzleOptions, // the modified options passed to Razzle in the `options` key in `razzle.config.js` (options: { key: 'value'})
+      webpackOptions, // the modified options that was used to configure webpack/ webpack loaders and plugins
+    },
+    paths,
+  }) {
     const themeConfigPath = `${__dirname}/src/theme/theme.config`;
     webpackConfig.resolve.alias['../../theme.config$'] = themeConfigPath;
     webpackConfig.resolve.alias['../../theme.config'] = themeConfigPath;
