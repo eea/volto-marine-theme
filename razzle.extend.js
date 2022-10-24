300814772bc2c11f4b2e339a17b26d79d22d9ea3
nileshgulia1 <nileshgulia@gmail.com>
2022-10-24 16:31:09 +0530

    pin to latest @plone/scripts

--- razzle.extend.js
+++ razzle.extend.js
@@ -1,15 +1,5 @@
 module.exports = {
-  modifyWebpackConfig({
-    env: { target, dev },
-    webpackConfig,
-    webpackObject,
-    options: {
-      pluginOptions,
-      razzleOptions, // the modified options passed to Razzle in the `options` key in `razzle.config.js` (options: { key: 'value'})
-      webpackOptions, // the modified options that was used to configure webpack/ webpack loaders and plugins
-    },
-    paths,
-  }) {
+  modifyWebpackConfig({ webpackConfig }) {
     const themeConfigPath = `${__dirname}/src/theme/theme.config`;
     webpackConfig.resolve.alias['../../theme.config$'] = themeConfigPath;
     webpackConfig.resolve.alias['../../theme.config'] = themeConfigPath;
