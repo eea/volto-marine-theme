7fe585683a723e48e71b2c40c36afe5381aef66b
nileshgulia1 <nileshgulia@gmail.com>
2022-11-23 16:28:38 +0530

    update customization

--- src/customizations/components/theme/App/App.jsx
+++ src/customizations/components/theme/App/App.jsx
@@ -49,8 +49,6 @@ import clearSVG from '@plone/volto/icons/clear.svg';
 import MultilingualRedirector from '@plone/volto/components/theme/MultilingualRedirector/MultilingualRedirector';
 import WorkingCopyToastsFactory from '@plone/volto/components/manage/WorkingCopyToastsFactory/WorkingCopyToastsFactory';
 
-import * as Sentry from '@sentry/browser';
-
 /**
  * @export
  * @class App
@@ -94,11 +92,7 @@ class App extends Component {
    */
   componentDidCatch(error, info) {
     this.setState({ hasError: true, error, errorInfo: info });
-    if (__CLIENT__) {
-      if (window?.env?.RAZZLE_SENTRY_DSN || __SENTRY__?.SENTRY_DSN) {
-        Sentry.captureException(error);
-      }
-    }
+    config.settings.errorHandlers.forEach((handler) => handler(error));
   }
 
   /**
