09b8f58f255a5dba4c319ac1f8051b1cd07820df
andreiggr <andrei.ggr@gmail.com>
2023-09-04 10:43:12 +0300

    fix: linted

--- src/config/facets.js
+++ src/config/facets.js
@@ -1,39 +1,40 @@
 import { multiTermFacet } from '@eeacms/search';
 import globalSearchBaseConfig from '@eeacms/volto-globalsearch/config/global-search-base-config.js';
 
-const facets = [...globalSearchBaseConfig.facets,
-multiTermFacet({
-  field: 'measure_sector.keyword',
-  isFilterable: false,
-  isMulti: true,
-  label: 'Sector',
-  iconsFamily: 'Sources',
-  alwaysVisible: true,
-}),
-multiTermFacet({
-  field: 'ecosystem_services.keyword',
-  isFilterable: false,
-  isMulti: true,
-  label: 'Ecosystem services',
-  iconsFamily: 'Sources',
-  alwaysVisible: true,
-}),
-multiTermFacet({
-  field: 'biophysical_impacts.keyword',
-  isFilterable: false,
-  isMulti: true,
-  label: 'Biophysical impacts',
-  iconsFamily: 'Sources',
-  alwaysVisible: true,
-}),
-multiTermFacet({
-  field: 'policy_objectives.keyword',
-  isFilterable: false,
-  isMulti: true,
-  label: 'Policy objectives',
-  iconsFamily: 'Sources',
-  alwaysVisible: true,
-}),
+const facets = [
+  ...globalSearchBaseConfig.facets,
+  multiTermFacet({
+    field: 'measure_sector.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Sector',
+    iconsFamily: 'Sources',
+    alwaysVisible: true,
+  }),
+  multiTermFacet({
+    field: 'ecosystem_services.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Ecosystem services',
+    iconsFamily: 'Sources',
+    alwaysVisible: true,
+  }),
+  multiTermFacet({
+    field: 'biophysical_impacts.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Biophysical impacts',
+    iconsFamily: 'Sources',
+    alwaysVisible: true,
+  }),
+  multiTermFacet({
+    field: 'policy_objectives.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Policy objectives',
+    iconsFamily: 'Sources',
+    alwaysVisible: true,
+  }),
 ];
 
 export default {
