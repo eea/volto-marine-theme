6f483d5d650f503b2273c1dcc7fd14dd48934ab7
laszlocseh <csehlaszlo91@gmail.com>
2023-10-24 16:50:29 +0300

    Remove tabs block, tableau, image cards customization

--- src/components/Blocks/CustomBlockTemplates/VoltoTableauBlock/index.js
+++
@@ -1,6 +0,0 @@
-export TableauView from './View';
-export TableauEdit from './Edit';
-export TableauShare from './TableauShare';
-export TableauDownload from './TableauDownload';
-export TableauFullscreen from './TableauFullscreen';
-export TableauPlaceholder from './TableauPlaceholder';
--- src/components/Blocks/CustomBlockTemplates/customBlockTemplates.js
+++ src/components/Blocks/CustomBlockTemplates/customBlockTemplates.js
@@ -1,19 +1,4 @@
 import {
-  PlainCardsView,
-  PlainCardsSchemaExtender,
-  ColoredCardsView,
-  ColoredCardsSchemaExtender,
-  PublicationCardsView,
-  PublicationCardsSchemaExtender,
-  PresentationCardsView,
-  PresentationCardsSchemaExtender,
-} from '@eeacms/volto-marine-theme/components/Blocks/CustomBlockTemplates/VoltoBlockImageCards';
-import {
-  TableauView,
-  TableauEdit,
-} from '@eeacms/volto-marine-theme/components/Blocks/CustomBlockTemplates/VoltoTableauBlock';
-// import { ColoredTabsView } from '@eeacms/volto-marine-theme/components/Blocks/CustomBlockTemplates/VoltoTabsBlock';
-import {
   BlockStyleWrapperEdit,
   BlockStyleWrapperView,
 } from '@eeacms/volto-block-style/BlockStyleWrapper';
@@ -21,54 +6,6 @@ import { ViewIframe, EditIframe } from '@eeacms/volto-embed/Iframe';
 
 const customBlockTemplates = (config) => ({
   ...config.blocks.blocksConfig,
-  imagecards: {
-    sidebarTab: 1,
-    ...config.blocks.blocksConfig.imagecards,
-    display_types: {
-      ...config.blocks.blocksConfig.imagecards?.display_types,
-    },
-    blockRenderers: {
-      plain_cards: {
-        title: 'Plain cards',
-        view: PlainCardsView,
-        schemaExtender: PlainCardsSchemaExtender,
-      },
-      colored_cards: {
-        title: 'Colored cards',
-        view: ColoredCardsView,
-        schemaExtender: ColoredCardsSchemaExtender,
-      },
-      publication_cards: {
-        title: 'Publication cards',
-        view: PublicationCardsView,
-        schemaExtender: PublicationCardsSchemaExtender,
-      },
-      presentation_cards: {
-        title: 'Pesentation cards',
-        view: PresentationCardsView,
-        schemaExtender: PresentationCardsSchemaExtender,
-      },
-      ...config.blocks.blocksConfig.imagecards?.blockRenderers,
-    },
-  },
-
-  tableau_block: {
-    ...config.blocks.blocksConfig.tableau_block,
-    view: TableauView,
-    edit: TableauEdit,
-  },
-
-  // tabs_block: {
-  //   ...(config.blocks.blocksConfig.tabs_block || {}),
-  //   templates: {
-  //     ...(config.blocks.blocksConfig.tabs_block?.templates || {}),
-  //     colored_tabs: {
-  //       ...config.blocks.blocksConfig.tabs_block.templates.default,
-  //       title: 'Colored tabs',
-  //       view: ColoredTabsView,
-  //     },
-  //   },
-  // },
 
   maps: {
     ...config.blocks.blocksConfig.maps,
